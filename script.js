1c2c6e1565b091e53b448e3d34bd8c33332e585a
hrishikesh599 <hrishikesht599@gmail.com>
2026-05-31 17:47:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -167,4 +167,15 @@ function animate(){
 }
 
 animate();
-console.log("working")
+window.addEventListener("load", () => {
+  const loading = document.getElementById("loadingScreen");
+
+  setTimeout(() => {
+    loading.style.opacity = "0";
+
+    setTimeout(() => {
+      loading.style.display = "none";
+    }, 800);
+
+  }, 1000);
+});
