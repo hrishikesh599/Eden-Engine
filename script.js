a3555a816a824c0d1aa0e41ae983e2387ab8878e
hrishikesh599 <hrishikesht599@gmail.com>
2026-06-01 11:18:44 +0530

    h

--- script.js
+++ script.js
@@ -700,17 +700,31 @@ class Plant {
 
         if(i > 2){
 
-            drawLeaf(
+    const side =
+        i % 2 === 0 ? 1 : -1;
 
-                seg.endX,
-                seg.endY,
+    const offset = 10;
 
-                seg.renderAngle,
+    drawLeaf(
 
-                8 + Math.sin(i)*2
+        seg.endX +
+        Math.cos(
+            seg.renderAngle +
+            side * Math.PI/2
+        ) * offset,
 
-            );
-        }
+        seg.endY +
+        Math.sin(
+            seg.renderAngle +
+            side * Math.PI/2
+        ) * offset,
+
+        seg.renderAngle +
+        side * 0.5,
+
+        14
+    );
+}
     });
 }
 }
